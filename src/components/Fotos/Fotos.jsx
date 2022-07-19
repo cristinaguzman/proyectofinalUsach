f1f186a7a17b16ce9bb18a1184a57a3060e39cee
Criss <cristinaguzman@live.cl>
2022-07-19 17:55:43 -0400

    proyecto final usach

--- src/components/Fotos/Fotos.jsx
+++ src/components/Fotos/Fotos.jsx
@@ -1,34 +1,58 @@
 import React, {useEffect, useState} from 'react'
 
-const [dataa, setdataa] = useState(null);
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+function HeaderAndFooterExample() {
+  return (
+    <Card className="text-center">
+      <Card.Header>Featured</Card.Header>
+      <Card.Body>
+        <Card.Title>Special title treatment</Card.Title>
+        <Card.Text>
+          With supporting text below as a natural lead-in to additional content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+      <Card.Footer className="text-muted">2 days ago</Card.Footer>
+    </Card>
+  );
+}
+
+export default HeaderAndFooterExample;
 
 export function Fotos() {
+
+  const [dataa, setdataa] = useState(null);
   useEffect(() => {
-    fetch("https://api.artic.edu/api/v1/artworks")
-        .then((response) => response.json())
-        .then((data) => {
-            const Data = {
-              img: "https://www.artic.edu/iiif/2/"+data.data[0].image_id+"/full/843,/0/default.jpg",
-              title: data.data[0].title
-            }
-          
-            setdataa(Data);
-       
-        })
+ 
+        async function getdata() {
+          const response = await fetch('https://api.artic.edu/api/v1/artworks', {
+            method: 'GET',
+            headers: {
+              accept: 'application/json',
+            },
+          });
+          const data = await response.json();        
+          setdataa(data.data);
+          console.log(setdataa)
+        }
+    
+        getdata();
     
       }, []);
-      <div>
-        <div className="card">
+      
 return (
-<div>
- 
-  {dataa ? (<div className='card w-100 justify-content-center align-items-center p-3'>  
-  <img width="200px" src={dataa.img}s alt="profile"/>
-  <p className='text-uppercase'>{dataa.title}</p> 
-  </div>):null}
-</div>
+  <div >
+  
+  {Array.isArray(dataa)
+        ? dataa.map(data => (
+            <div key={data.id}>
+              <h5>Título de la obra: {data.title} </h5>
+             <img width="20%" alt='a' src={`https://www.artic.edu/iiif/2/`+data.image_id+`/full/843,/0/default.jpg`}/>
+            </div>
+          ))
+        : null}
+  </div>
 )
-</div>
-</div>
-
-  }
+}
